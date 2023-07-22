c763e4986c6da4dbf5b7cfef6ecbf5f7805e1e3a
JohnnHidalgo <johnna.7h@gmail.com>
2023-07-22 02:12:19 -0400

    Paso 3

--- lib/title_screen/title_screen.dart
+++ lib/title_screen/title_screen.dart
@@ -5,12 +5,20 @@
 import 'package:flutter/material.dart';
 
 import '../assets.dart';
+import '../styles.dart';
+import 'title_screen_ui.dart';
 
 class TitleScreen extends StatelessWidget {
   const TitleScreen({super.key});
 
+  final _finalReceiveLightAmt = 0.7;
+  final _finalEmitLightAmt = 0.5;
+
   @override
   Widget build(BuildContext context) {
+    final orbColor = AppColors.orbColors[0];
+    final emitColor = AppColors.emitColors[0];
+
     return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
@@ -20,28 +28,78 @@ class TitleScreen extends StatelessWidget {
             Image.asset(AssetPaths.titleBgBase),
 
             /// Bg-Receive
-            Image.asset(AssetPaths.titleBgReceive),
+            _LitImage(
+              color: orbColor,
+              imgSrc: AssetPaths.titleBgReceive,
+              lightAmt: _finalReceiveLightAmt,
+            ),
 
             /// Mg-Base
-            Image.asset(AssetPaths.titleMgBase),
+            _LitImage(
+              imgSrc: AssetPaths.titleMgBase,
+              color: orbColor,
+              lightAmt: _finalReceiveLightAmt,
+            ),
 
             /// Mg-Receive
-            Image.asset(AssetPaths.titleMgReceive),
+            _LitImage(
+              imgSrc: AssetPaths.titleMgReceive,
+              color: orbColor,
+              lightAmt: _finalReceiveLightAmt,
+            ),
 
             /// Mg-Emit
-            Image.asset(AssetPaths.titleMgEmit),
+            _LitImage(
+              imgSrc: AssetPaths.titleMgEmit,
+              color: emitColor,
+              lightAmt: _finalEmitLightAmt,
+            ),
 
             /// Fg-Rocks
             Image.asset(AssetPaths.titleFgBase),
 
             /// Fg-Receive
-            Image.asset(AssetPaths.titleFgReceive),
+            _LitImage(
+              imgSrc: AssetPaths.titleFgReceive,
+              color: orbColor,
+              lightAmt: _finalReceiveLightAmt,
+            ),
 
             /// Fg-Emit
-            Image.asset(AssetPaths.titleFgEmit),
+            _LitImage(
+              imgSrc: AssetPaths.titleFgEmit,
+              color: emitColor,
+              lightAmt: _finalEmitLightAmt,
+            ),
+
+            /// UI
+            const Positioned.fill(
+              child: TitleScreenUi(),
+            ),
           ],
         ),
       ),
+    );
+  }
+}
+
+class _LitImage extends StatelessWidget {
+  const _LitImage({
+    required this.color,
+    required this.imgSrc,
+    required this.lightAmt,
+  });
+  final Color color;
+  final String imgSrc;
+  final double lightAmt;
+
+  @override
+  Widget build(BuildContext context) {
+    final hsl = HSLColor.fromColor(color);
+    return Image.asset(
+      imgSrc,
+      color: hsl.withLightness(hsl.lightness * lightAmt).toColor(),
+      colorBlendMode: BlendMode.modulate,
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,13 +7,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:next_gen_ui/main.dart';
 
-import 'package:google_io_extended_la_paz_2023/main.dart';
+//import 'package:google_io_extended_la_paz_2023/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const NextGenApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
